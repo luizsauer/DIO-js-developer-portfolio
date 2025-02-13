f51767291aabb340275555729467d7ae88c5dcfb
luizsauer <luizsauer@gmail.com>
2025-02-12 22:55:34 -0300

    att json

--- assets/js/main.js
+++ assets/js/main.js
@@ -20,11 +20,38 @@ function updateProfileInfo(profileData) {
     const email = document.getElementById('profile.email')
     email.innerText = profileData.email
     phone.href = `mailto:${profileData.email}`
+}
 
+function updateSoftSkills(profileData) {
+    const softSkills = document.getElementById('profile.skills.softSkills')
+    softSkills.innerHTML = profileData.skills.softSkills.map(skill => `<li>${skill}</li>`).join('')
+}
+function updateHardSkills(profileData) {
+    const hardSkills = document.getElementById('profile.skills.hardSkills')
+    hardSkills.innerHTML = profileData.skills.hardSkills.map(skill => `<li><img src="${skill.logo}" alt="${skill.name}" title="${skill.name}"></li>`).join('')
+}
+function updateLanguages(profileData) {
+    const languages = document.getElementById('profile.languages')
+    languages.innerHTML = profileData.languages.map(language => `<li>${language}</li>`).join('')
+}
+
+function updatePortifolio(profileData) {
+    const portifolio = document.getElementById('profile.portfolio')
+    portifolio.innerHTML = profileData.portfolio.map(portfolio => {
+        return `
+            <li>
+                <h3 ${portfolio.github ? 'class="title github"' : ''}>${portfolio.name}</h3>
+                <a href="${portfolio.url}" target="_blank">${portfolio.url}</a>
+            </li>
+            `}).join('')
 }
 
 
 (async () => {
     const profileData = await fetchProfileData() //fazendo a busca
     updateProfileInfo(profileData)
+    updateSoftSkills(profileData)
+    updateHardSkills(profileData)
+    updateLanguages(profileData)
+    updatePortifolio(profileData)
 })()
